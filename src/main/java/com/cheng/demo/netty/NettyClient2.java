fe7668e721f96e25b8d94e0411a1784383addb3f
cheng <792702352@qq.com>
2018-12-05 18:14:40 +0800

    :sparkles: netty 客户端服务端初识

---
+++ src/main/java/com/cheng/demo/netty/NettyClient2.java
@@ -0,0 +1,96 @@
+package com.cheng.demo.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author cheng
+ *         2018/12/5 17:48
+ */
+public class NettyClient2 {
+
+    /**
+     * 最大尝试重连次数
+     */
+    private static final int MAX_RETRY = 5;
+
+    public static void main(String[] args) {
+
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap
+                // 1. 指定线程模型
+                .group(workerGroup)
+                // 2. 指定 IO 类型为 NIO
+                .channel(NioSocketChannel.class)
+                // 3. IO 处理逻辑
+                .handler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    public void initChannel(SocketChannel ch) {
+                    }
+                });
+
+        // 4. 建立连接
+//        bootstrap.connect("www.chengix.com", 80).addListener(future -> {
+//            if (future.isSuccess()) {
+//                System.out.println("连接成功!");
+//            } else {
+//                System.err.println("连接失败!");
+//            }
+//        });
+
+        // 失败重连
+//        connect(bootstrap, "127.0.0.1", 777);
+
+        // 指定最大重试次数
+//        connect(bootstrap, "127.0.0.1", 777, MAX_RETRY);
+
+        // attr() 方法
+//        bootstrap.attr(AttributeKey.newInstance("clientName"), "nettyClient");
+
+        // option() 方法
+        bootstrap
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .option(ChannelOption.SO_KEEPALIVE, true)
+                .option(ChannelOption.TCP_NODELAY, true);
+    }
+
+    private static void connect(Bootstrap bootstrap, String host, int port) {
+        bootstrap.connect(host, port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("连接成功!");
+            } else {
+                System.err.println("连接失败，开始重连!");
+                connect(bootstrap, host, port);
+            }
+        });
+    }
+
+    private static void connect(Bootstrap bootstrap, String host, int port, int retry) {
+        bootstrap.connect(host, port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("连接成功!");
+            } else if (retry == 0) {
+                System.err.println("重试次数已用完，放弃连接!");
+            } else {
+                // 第几次重连
+                int order = (MAX_RETRY - retry) + 1;
+
+                // 本次重连的间隔
+                int delay = 1 << order;
+                System.err.println(new Date() + ": 连接失败，第" + order + "次重连……");
+
+                bootstrap.config().group().schedule(() ->
+                        connect(bootstrap, host, port, retry - 1), delay, TimeUnit.SECONDS);
+            }
+        });
+    }
+}
---
+++ src/main/java/com/cheng/demo/netty/NettyServer2.java
@@ -0,0 +1,66 @@
+package com.cheng.demo.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ *
+ * @author cheng
+ *         2018/12/5 17:21
+ */
+public class NettyServer2 {
+    public static void main(String[] args) {
+
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+        serverBootstrap
+                .group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    protected void initChannel(NioSocketChannel ch) {
+                    }
+                });
+
+        // 自动绑定递增端口
+        bind(serverBootstrap, 1000);
+
+        // 服务端启动其他方法
+//        serverBootstrap.handler(new ChannelInitializer<NioSocketChannel>() {
+//            @Override
+//            protected void initChannel(NioSocketChannel ch) throws Exception {
+//                System.out.println("服务端启动中");
+//            }
+//        });
+
+        // attr() 方法
+//        serverBootstrap.attr(AttributeKey.newInstance("serverName"), "nettyServer");
+
+        // childAttr() 方法
+//        serverBootstrap.childAttr(AttributeKey.newInstance("clientKey"), "clientValue");
+
+        // childOption() 方法
+//        serverBootstrap
+//                .childOption(ChannelOption.SO_KEEPALIVE, true)
+//                .childOption(ChannelOption.TCP_NODELAY, true);
+
+        // option() 方法
+//        serverBootstrap.option(ChannelOption.SO_BACKLOG, 1024);
+    }
+
+    private static void bind(final ServerBootstrap serverBootstrap, final int port) {
+        serverBootstrap.bind(port).addListener(future -> {
+            if (future.isSuccess()) {
+                System.out.println("端口[" + port + "]绑定成功!");
+            } else {
+                System.err.println("端口[" + port + "]绑定失败!");
+                bind(serverBootstrap, port + 1);
+            }
+        });
+    }
+}
